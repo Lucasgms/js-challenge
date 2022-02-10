2b2fae7aab01958dc04a14522bc05874a97f87fb
Lucas Gregory <lgregory12@hotmail.com>
2022-02-10 08:34:04 -0300

    feat: solutions implemented
    two problems solved

---
+++ main.ts
@@ -0,0 +1,18 @@
+function nonConstructibleChange (coins: number[]): number {
+  const sortedArray = coins.sort((a, b) => { return b - a });
+  
+  if (sortedArray.length > 1 && sortedArray[0] !== sortedArray[1]) {
+  	  sortedArray.shift();
+  }
+  
+  const sumAmount = sortedArray.reduce((current, amount) => { return current + amount }, 0);
+  
+  return sumAmount + 1;
+}
+
+
+function sortedSquaredArray (numericList: number[]): number[] {
+  const squaredArray = numericList.map(item => { return item * item }).sort((a, b) => { return a - b });
+    
+  return squaredArray;
+}
